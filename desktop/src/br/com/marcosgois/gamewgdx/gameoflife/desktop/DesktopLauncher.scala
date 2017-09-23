84a1e4be73e28dbf48159452684473e96f300b5f
marcosgois <goisinho_5@hotmail.com>
2017-09-23 01:17:30 -0300

    Renomeando o jogo e atualizando renderização

--- desktop/src/br/com/marcosgois/gamewgdx/gameoflife/desktop/DesktopLauncher.scala
+++ desktop/src/br/com/marcosgois/gamewgdx/gameoflife/desktop/DesktopLauncher.scala
@@ -8,9 +8,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 object DesktopLauncher {
 		def main(arg: Array[String]): Unit = {
 			val config = new LwjglApplicationConfiguration
-			config.title = "MARILIA EU TE AMO"
+			config.title = "DROP AND CATCH THE DROPLET"
 			config.height = 480
-			config.width = 480
+			config.width = 800
 			new LwjglApplication(new GameOfLife(config.width,config.height), config)
 		}
 		}
